c7eed09aa5af9ce9ba79b1bf372dc885d992052a
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-18 07:45:03 +0530

    refactor: clean settings and payment flows

--- ATM/NearbyATM.cs
+++ ATM/NearbyATM.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
+using System;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
 using DGVPrinterHelper;
 
 namespace ATM
@@ -21,90 +16,75 @@ namespace ATM
 
         private void txtsearchbar_TextChanged(object sender, EventArgs e)
         {
-            if (txtsearchbar.Text != "")
-            {
-                string connectionString;
-                SqlConnection cnn;
-
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-                cnn = new SqlConnection(connectionString);
-                cnn.Open();
-                String sql = "Select*from NearbyATMs where ATMNo LIKE '" + txtsearchbar.Text + "%'";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-
-                SqlDataAdapter ada = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                ada.Fill(dataTable);
-                dataGridView1.DataSource = dataTable;
-                cnn.Close();
-            }
-            else
-            {
-                string connectionString;
-                SqlConnection cnn;
-
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-                cnn = new SqlConnection(connectionString);
-                cnn.Open();
-                String sql = "Select*from NearbyATMs ";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-
-                SqlDataAdapter ada = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                ada.Fill(dataTable);
-                dataGridView1.DataSource = dataTable;
-                cnn.Close();
-            }
+            LoadAtms(txtsearchbar.Text.Trim());
         }
 
         private void btnrefresh_Click(object sender, EventArgs e)
         {
             txtsearchbar.Clear();
+            LoadAtms(string.Empty);
         }
 
         private void btnback_Click(object sender, EventArgs e)
         {
-            this.Hide();
+            Hide();
         }
 
         private void btnprint_Click(object sender, EventArgs e)
         {
-            DGVPrinter printer = new DGVPrinter();
-            printer.Title = "\r\n\r\n\r\n ZEMO Bank \r\n\r\n";
-            printer.SubTitle = "Nearby ATMs \r\n\r\n ( Distance is in Km and the starting point is our Galle branch ) \r\n\r\n\r\n";
-            printer.SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip;
-            printer.PageNumbers = false;
-            printer.PageNumberInHeader = false;
-            printer.PorportionalColumns = true;
-            printer.HeaderCellAlignment = StringAlignment.Near;
-            printer.Footer = "Thank you - ZEMO Bank";
-            printer.FooterSpacing = 15;
+            DGVPrinter printer = new DGVPrinter
+            {
+                Title = "\r\n\r\n\r\n ZEMO Bank \r\n\r\n",
+                SubTitle = "Nearby ATMs \r\n\r\n (Distance is in Km and the starting point is our Galle branch) \r\n\r\n\r\n",
+                SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip,
+                PageNumbers = false,
+                PageNumberInHeader = false,
+                PorportionalColumns = true,
+                HeaderCellAlignment = StringAlignment.Near,
+                Footer = "Thank you - ZEMO Bank",
+                FooterSpacing = 15
+            };
+
             printer.PrintDataGridView(dataGridView1);
-
-            MessageBox.Show("Successfully printed", "Settings and Services", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-            this.Hide();
+            MessageBox.Show("Successfully printed.", "Settings and Services",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Hide();
         }
 
         private void NearbyATM_Load(object sender, EventArgs e)
         {
-            string connectionString;
-            SqlConnection cnn;
+            LoadAtms(string.Empty);
+        }
 
-            connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
+        private void LoadAtms(string prefix)
+        {
+            try
+            {
+                using (SqlConnection connection = Database.OpenConnection())
+                using (SqlCommand command = new SqlCommand(
+                    string.IsNullOrWhiteSpace(prefix)
+                        ? "SELECT ATMNo, Branch, Location, Distance FROM NearbyATMs ORDER BY ATMNo"
+                        : "SELECT ATMNo, Branch, Location, Distance FROM NearbyATMs WHERE ATMNo LIKE @Prefix ORDER BY ATMNo",
+                    connection))
+                {
+                    if (!string.IsNullOrWhiteSpace(prefix))
+                    {
+                        command.Parameters.AddWithValue("@Prefix", prefix + "%");
+                    }
 
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            String sql = "Select*from NearbyATMs ";
-            SqlCommand cmd = new SqlCommand(sql, cnn);
-
-            SqlDataAdapter ada = new SqlDataAdapter(cmd);
-            DataTable dataTable = new DataTable();
-            ada.Fill(dataTable);
-            dataGridView1.DataSource = dataTable;
-            cnn.Close();
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+                        dataGridView1.DataSource = table;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ATM locations could not be loaded. " + ex.Message,
+                    "Nearby ATMs", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
