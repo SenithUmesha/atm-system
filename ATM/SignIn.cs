ad9c6f3c41c423e72f229cd7bf560701da6f2ea6
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-18 07:43:28 +0530

    refactor: make account transactions safer

--- ATM/SignIn.cs
+++ ATM/SignIn.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ATM
@@ -15,6 +8,7 @@ namespace ATM
     {
         public static string signinName;
         public static string signinPin;
+        public static string signinAccNo;
         public static int type;
 
         public Home()
@@ -27,72 +21,155 @@ namespace ATM
 
         private void pbclose_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void btnsignin_Click(object sender, EventArgs e)
         {
-            if (type == 0)
+            string accountName = txtaccname.Text.Trim();
+            string credential = txtpin.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(accountName) || string.IsNullOrWhiteSpace(credential))
             {
-                string connectionString;
-                SqlConnection cnn;
+                MessageBox.Show("Account name and credential are required.", "Sign in",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
 
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-                cnn = new SqlConnection(connectionString);
-                cnn.Open();
-                string sql = "Select * from UserAccountDetails where Name = '" + txtaccname.Text + "' and Pin = '" + txtpin.Text + "'";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-                SqlDataReader reader = cmd.ExecuteReader();
-
-                if (reader.Read())
+            try
+            {
+                if (type == 0)
                 {
-                    if (MessageBox.Show("Login Successfully", "User Login", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                    if (!TryAuthenticateCustomer(accountName, credential, out string accountNumber))
                     {
-                        signinName = txtaccname.Text;
-                        signinPin = txtpin.Text;
+                        MessageBox.Show("Either your account name or PIN number is incorrect.", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
 
-                        this.Hide();
-                        Dash1 dash = new Dash1();
+                    signinName = accountName;
+                    signinPin = credential;
+                    signinAccNo = accountNumber;
+
+                    Hide();
+                    using (Dash1 dash = new Dash1())
+                    {
                         dash.ShowDialog();
                     }
+                    Show();
                 }
-                else
+                else if (type == 1)
                 {
-                    MessageBox.Show("Either your account name or pin number is incorrect", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                cnn.Close();
-            }
-            else if(type == 1) {
-
-                string connectionString;
-                SqlConnection cnn;
-
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-                cnn = new SqlConnection(connectionString);
-                cnn.Open();
-                string sql = "Select * from ManagementAccountDetails where Name = '" + txtaccname.Text + "' and Password = '" + txtpin.Text + "'";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-                SqlDataReader reader = cmd.ExecuteReader();
-
-                if (reader.Read())
-                {
-                    if (MessageBox.Show("Login Successfully", "Management Login", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                    if (!TryAuthenticateManager(accountName, credential))
                     {
-                        signinName = txtaccname.Text;
-                        signinPin = txtpin.Text;
+                        MessageBox.Show("Either your account name or password is incorrect.", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
 
-                        this.Hide();
-                        AdminDash Adash = new AdminDash();
-                        Adash.ShowDialog();
+                    signinName = accountName;
+                    signinPin = null;
+                    signinAccNo = null;
+
+                    Hide();
+                    using (AdminDash adminDash = new AdminDash())
+                    {
+                        adminDash.ShowDialog();
+                    }
+                    Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Sign in could not be completed. " + ex.Message, "Sign in",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private static bool TryAuthenticateCustomer(string name, string pin, out string accountNumber)
+        {
+            accountNumber = null;
+
+            using (SqlConnection connection = Database.OpenConnection())
+            using (SqlCommand command = new SqlCommand(
+                "SELECT AccNo, Pin FROM UserAccountDetails WHERE Name = @Name",
+                connection))
+            {
+                command.Parameters.AddWithValue("@Name", name);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string storedPin = Convert.ToString(reader["Pin"]);
+                        if (CredentialHasher.Verify(pin, storedPin, out bool legacyPlaintext))
+                        {
+                            accountNumber = Convert.ToString(reader["AccNo"]);
+                            reader.Close();
+
+                            if (legacyPlaintext)
+                            {
+                                UpgradeCustomerPin(connection, accountNumber, pin);
+                            }
+
+                            return true;
+                        }
                     }
                 }
-                else
+            }
+
+            return false;
+        }
+
+        private static bool TryAuthenticateManager(string name, string password)
+        {
+            using (SqlConnection connection = Database.OpenConnection())
+            using (SqlCommand command = new SqlCommand(
+                "SELECT AccNo, Password FROM ManagementAccountDetails WHERE Name = @Name",
+                connection))
+            {
+                command.Parameters.AddWithValue("@Name", name);
+
+                using (SqlDataReader reader = command.ExecuteReader())
                 {
-                    MessageBox.Show("Either your account name or password is incorrect", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    while (reader.Read())
+                    {
+                        string storedPassword = Convert.ToString(reader["Password"]);
+                        if (CredentialHasher.Verify(password, storedPassword, out bool legacyPlaintext))
+                        {
+                            string accountNumber = Convert.ToString(reader["AccNo"]);
+                            reader.Close();
+
+                            if (legacyPlaintext)
+                            {
+                                using (SqlCommand update = new SqlCommand(
+                                    "UPDATE ManagementAccountDetails SET Password = @Password WHERE AccNo = @AccNo",
+                                    connection))
+                                {
+                                    update.Parameters.AddWithValue("@Password", CredentialHasher.Hash(password));
+                                    update.Parameters.AddWithValue("@AccNo", accountNumber);
+                                    update.ExecuteNonQuery();
+                                }
+                            }
+
+                            return true;
+                        }
+                    }
                 }
-                cnn.Close();
+            }
+
+            return false;
+        }
+
+        private static void UpgradeCustomerPin(SqlConnection connection, string accountNumber, string pin)
+        {
+            using (SqlCommand update = new SqlCommand(
+                "UPDATE UserAccountDetails SET Pin = @Pin WHERE AccNo = @AccNo",
+                connection))
+            {
+                update.Parameters.AddWithValue("@Pin", CredentialHasher.Hash(pin));
+                update.Parameters.AddWithValue("@AccNo", accountNumber);
+                update.ExecuteNonQuery();
             }
         }
 
@@ -124,14 +201,7 @@ namespace ATM
         {
             if (txtpin.Text.Equals(""))
             {
-                if (type == 0)
-                {
-                    txtpin.Text = "     PIN Number";
-                }
-                else 
-                {
-                    txtpin.Text = "     Password";
-                }
+                txtpin.Text = type == 0 ? "     PIN Number" : "     Password";
             }
         }
 
@@ -139,21 +209,12 @@ namespace ATM
         {
             if (comboBox1.SelectedIndex == 0 || comboBox1.SelectedIndex == 1)
             {
-                if (comboBox1.SelectedIndex == 1)
-                {
-                    txtpin.Text = "     Password";
-                }
-                else 
-                {
-                    txtpin.Text = "     PIN Number";
-                }
-
+                type = comboBox1.SelectedIndex;
+                txtpin.Text = type == 0 ? "     PIN Number" : "     Password";
                 txtaccname.Visible = true;
                 txtpin.Visible = true;
                 btnsignin.Visible = true;
             }
-
-            type = comboBox1.SelectedIndex;
         }
     }
 }
