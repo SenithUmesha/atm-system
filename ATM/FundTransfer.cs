a7fd5aff41b6aab0e6d212bde5970c80add6527d
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-18 07:43:53 +0530

    refactor: make account transactions safer

--- ATM/FundTransfer.cs
+++ ATM/FundTransfer.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
 namespace ATM
 {
     public partial class FundTransfer : Form
     {
-        public static double Balance, NewBalance;
-        public static string type = "Fund Transfer";
-
         public FundTransfer()
         {
             InitializeComponent();
@@ -23,98 +12,62 @@ namespace ATM
 
         private void btnproceed_Click(object sender, EventArgs e)
         {
-            DateTime time = DateTime.Now;
-            string format = "yyyy-MM-dd HH:mm:ss";
+            string recipient = txtraccno.Text.Trim();
 
-            if (txtamount.Text == "" || txtcpin.Text == "" || txtraccno.Text == "")
+            if (string.IsNullOrWhiteSpace(recipient)
+                || string.IsNullOrWhiteSpace(txtamount.Text)
+                || string.IsNullOrWhiteSpace(txtcpin.Text))
             {
-                MessageBox.Show("Fields can't be empty", "Bill Payment", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Fields can't be empty.", "Fund Transfer",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
             }
-            else
+
+            if (txtcpin.Text != Home.signinPin)
             {
-                double amount = Convert.ToDouble(txtamount.Text);
+                MessageBox.Show("Incorrect PIN number.", "Fund Transfer",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
 
-                if (amount <= 0)
+            if (!decimal.TryParse(txtamount.Text, out decimal amount) || amount <= 0)
+            {
+                MessageBox.Show("Enter an amount greater than zero.", "Fund Transfer",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                if (!BankingService.TryTransfer(
+                    Dash1.AccNo,
+                    recipient,
+                    amount,
+                    DateTime.Now,
+                    out decimal _,
+                    out string error))
                 {
-                    MessageBox.Show("Amount can't be 0", "Bill Payment", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                else
-                {
-                    string connectionString;
-                    SqlConnection cnn;
-
-                    connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-                    cnn = new SqlConnection(connectionString);
-                    cnn.Open();
-                    string sql1 = "Select Balance from UserAccountDetails where Pin = '" + Home.signinPin + "'";
-                    SqlCommand cmd1 = new SqlCommand(sql1, cnn);
-
-                    using (SqlDataReader reader1 = cmd1.ExecuteReader())
-                    {
-                        while (reader1.Read())
-                        {
-                            Balance = Convert.ToDouble(reader1["Balance"]);
-                        }
-                    }
-                    cnn.Close();
-
-                    if (Balance < amount)
-                    {
-                        MessageBox.Show("Insufficient balance", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }
-                    else
-                    {
-                        if (txtcpin.Text == Home.signinPin)
-                        {
-                            NewBalance = Balance - amount;
-
-                            cnn = new SqlConnection(connectionString);
-                            cnn.Open();
-
-                            string sql2 = "Update UserAccountDetails set Balance='" + NewBalance + "' where Pin = '" + Home.signinPin + "' ";
-                            SqlCommand cmd2 = new SqlCommand(sql2, cnn);
-                            cmd2.ExecuteNonQuery();
-
-                            cnn.Close();
-
-                            cnn = new SqlConnection(connectionString);
-                            cnn.Open();
-
-                            string sql3 = "INSERT INTO RecentT(AccNo,Type,Date,Amount) VALUES('" + Dash1.AccNo + "','" + type + "','" + time.ToString(format) + "','" + amount + "')";
-                            SqlCommand cmd3 = new SqlCommand(sql3, cnn);
-                            cmd3.ExecuteNonQuery();
-
-                            cnn.Close();
-
-                            cnn = new SqlConnection(connectionString);
-                            cnn.Open();
-
-                            string sql4 = "INSERT INTO FundTransfer(AccNo,RAccNo,Date,Amount) VALUES('" + Dash1.AccNo + "','" + txtraccno.Text + "','" + time.ToString(format) + "','" + amount + "')";
-                            SqlCommand cmd4 = new SqlCommand(sql4, cnn);
-                            cmd4.ExecuteNonQuery();
-
-                            cnn.Close();
-
-                            MessageBox.Show("Transfer completed", "Fund Transfer", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                            this.Hide();
-                            Dash1 dash = new Dash1();
-                            dash.ShowDialog();
-                        }
-                        else
-                        {
-                            MessageBox.Show("Incorrect PIN number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        }
-                    }
+                    MessageBox.Show(error, "Fund Transfer",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
                 }
 
+                MessageBox.Show("Transfer completed.", "Fund Transfer",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Hide();
+                new Dash1().ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Transfer could not be completed. " + ex.Message, "Fund Transfer",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
         private void pbclose_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void btnreset_Click(object sender, EventArgs e)
@@ -126,39 +79,32 @@ namespace ATM
 
         private void txtraccno_KeyPress(object sender, KeyPressEventArgs e)
         {
-            char chr = e.KeyChar;
-            if (!char.IsDigit(chr) && chr != 8)
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
             {
                 e.Handled = true;
-                MessageBox.Show("Please Enter A Valid Value", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void txtamount_KeyPress(object sender, KeyPressEventArgs e)
         {
-            char chr = e.KeyChar;
-            if (!char.IsDigit(chr) && chr != 8 && chr != '.')
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != '.')
             {
                 e.Handled = true;
-                MessageBox.Show("Please Enter A Valid Value", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void txtcpin_KeyPress(object sender, KeyPressEventArgs e)
         {
-            char chr = e.KeyChar;
-            if (!char.IsDigit(chr) && chr != 8)
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
             {
                 e.Handled = true;
-                MessageBox.Show("Please Enter A Valid Value", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void btnback_Click(object sender, EventArgs e)
         {
-            this.Close();
-            Dash1 dash = new Dash1();
-            dash.Show();
+            Close();
+            new Dash1().Show();
         }
     }
 }
