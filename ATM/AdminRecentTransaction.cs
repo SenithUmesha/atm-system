8a4c8b978b29b23683103ca4c36d067ffa0bcb34
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-18 07:46:18 +0530

    refactor: clean admin data views

--- ATM/AdminRecentTransaction.cs
+++ ATM/AdminRecentTransaction.cs
@@ -1,13 +1,6 @@
-﻿using DGVPrinterHelper;
+using DGVPrinterHelper;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ATM
@@ -21,95 +14,65 @@ namespace ATM
 
         private void btnback_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            AdminDash adminDash = new AdminDash();
-            adminDash.ShowDialog();
+            Hide();
+            new AdminDash().ShowDialog();
         }
 
         private void AdminRecentTransaction_Load(object sender, EventArgs e)
         {
-            string connectionString;
-            SqlConnection cnn;
-
-            connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            String sql = "Select*from RecentT";
-            SqlCommand cmd = new SqlCommand(sql, cnn);
-
-            SqlDataAdapter ada = new SqlDataAdapter(cmd);
-            DataTable dataTable = new DataTable();
-            ada.Fill(dataTable);
-            dataGridView1.DataSource = dataTable;
-            cnn.Close();
+            LoadData();
         }
 
         private void btnprint_Click(object sender, EventArgs e)
         {
-            DGVPrinter printer = new DGVPrinter();
-            printer.Title = "\r\n\r\n\r\n ZEMO Bank \r\n\r\n";
-            printer.SubTitle = "Recent Transactions - Admin \r\n\r\n\r\n";
-            printer.SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip;
-            printer.PageNumbers = false;
-            printer.PageNumberInHeader = false;
-            printer.PorportionalColumns = true;
-            printer.HeaderCellAlignment = StringAlignment.Near;
-            printer.Footer = "Admin - ZEMO Bank";
-            printer.FooterSpacing = 15;
-            printer.PrintDataGridView(dataGridView1);
-
-            MessageBox.Show("Successfully printed", "Admin - Recent Transactions", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            PrintGrid("Recent Transactions - Admin", "Admin - Recent Transactions");
         }
 
         private void pbclose_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void btnrefresh_Click(object sender, EventArgs e)
         {
             txtsearchbar.Clear();
+            LoadData();
         }
 
         private void txtsearchbar_TextChanged(object sender, EventArgs e)
         {
-            if (txtsearchbar.Text != "")
+            LoadData(txtsearchbar.Text.Trim());
+        }
+
+        private void LoadData(string prefix = "")
+        {
+            try
             {
-                string connectionString;
-                SqlConnection cnn;
-
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-                cnn = new SqlConnection(connectionString);
-                cnn.Open();
-                String sql = "Select*from RecentT where AccNo LIKE '" + txtsearchbar.Text + "%'";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-
-                SqlDataAdapter ada = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                ada.Fill(dataTable);
-                dataGridView1.DataSource = dataTable;
-                cnn.Close();
+                dataGridView1.DataSource = GridData.LoadRecentTransactions(prefix);
             }
-            else
+            catch (Exception ex)
             {
-                string connectionString;
-                SqlConnection cnn;
-
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-                cnn = new SqlConnection(connectionString);
-                cnn.Open();
-                String sql = "Select*from RecentT";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-
-                SqlDataAdapter ada = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                ada.Fill(dataTable);
-                dataGridView1.DataSource = dataTable;
-                cnn.Close();
+                MessageBox.Show("Transactions could not be loaded. " + ex.Message,
+                    "Admin - Recent Transactions", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+        }
+
+        private void PrintGrid(string subtitle, string title)
+        {
+            DGVPrinter printer = new DGVPrinter
+            {
+                Title = "\r\n\r\n\r\n ZEMO Bank \r\n\r\n",
+                SubTitle = subtitle + " \r\n\r\n\r\n",
+                SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip,
+                PageNumbers = false,
+                PageNumberInHeader = false,
+                PorportionalColumns = true,
+                HeaderCellAlignment = StringAlignment.Near,
+                Footer = "Admin - ZEMO Bank",
+                FooterSpacing = 15
+            };
+            printer.PrintDataGridView(dataGridView1);
+            MessageBox.Show("Successfully printed.", title, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
