02ab705229001c7e99b0a81c568fd73dcdd05cc4
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-18 07:46:44 +0530

    refactor: clean admin data views

--- ATM/AdminNearbyATMs.cs
+++ ATM/AdminNearbyATMs.cs
@@ -1,13 +1,6 @@
-﻿using DGVPrinterHelper;
+using DGVPrinterHelper;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ATM
@@ -21,95 +14,66 @@ namespace ATM
 
         private void btnprint_Click(object sender, EventArgs e)
         {
-            DGVPrinter printer = new DGVPrinter();
-            printer.Title = "\r\n\r\n\r\n ZEMO Bank \r\n\r\n";
-            printer.SubTitle = "Nearby ATMs - Admin \r\n\r\n\r\n";
-            printer.SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip;
-            printer.PageNumbers = false;
-            printer.PageNumberInHeader = false;
-            printer.PorportionalColumns = true;
-            printer.HeaderCellAlignment = StringAlignment.Near;
-            printer.Footer = "Admin - ZEMO Bank";
-            printer.FooterSpacing = 15;
+            DGVPrinter printer = CreatePrinter("Nearby ATMs - Admin");
             printer.PrintDataGridView(dataGridView1);
-
-            MessageBox.Show("Successfully printed", "Admin - Nearby ATMs", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Successfully printed.", "Admin - Nearby ATMs",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void btnback_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            AdminDash adminDash = new AdminDash();
-            adminDash.ShowDialog();
+            Hide();
+            new AdminDash().ShowDialog();
         }
 
         private void AdminNearbyATMs_Load(object sender, EventArgs e)
         {
-            string connectionString;
-            SqlConnection cnn;
-
-            connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            String sql = "Select*from NearbyATMs ";
-            SqlCommand cmd = new SqlCommand(sql, cnn);
-
-            SqlDataAdapter ada = new SqlDataAdapter(cmd);
-            DataTable dataTable = new DataTable();
-            ada.Fill(dataTable);
-            dataGridView1.DataSource = dataTable;
-            cnn.Close();
+            LoadData();
         }
 
         private void pbclose_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void btnrefresh_Click(object sender, EventArgs e)
         {
             txtsearchbar.Clear();
+            LoadData();
         }
 
         private void txtsearchbar_TextChanged(object sender, EventArgs e)
         {
-            if (txtsearchbar.Text != "")
+            LoadData(txtsearchbar.Text.Trim());
+        }
+
+        private void LoadData(string prefix = "")
+        {
+            try
             {
-                string connectionString;
-                SqlConnection cnn;
-
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-                cnn = new SqlConnection(connectionString);
-                cnn.Open();
-                String sql = "Select*from NearbyATMs where ATMNo LIKE '" + txtsearchbar.Text + "%'";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-
-                SqlDataAdapter ada = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                ada.Fill(dataTable);
-                dataGridView1.DataSource = dataTable;
-                cnn.Close();
+                dataGridView1.DataSource = GridData.LoadNearbyAtms(prefix);
             }
-            else
+            catch (Exception ex)
             {
-                string connectionString;
-                SqlConnection cnn;
-
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZEMO_Bank;User ID=admin;Password=admin";
-
-                cnn = new SqlConnection(connectionString);
-                cnn.Open();
-                String sql = "Select*from NearbyATMs ";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-
-                SqlDataAdapter ada = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                ada.Fill(dataTable);
-                dataGridView1.DataSource = dataTable;
-                cnn.Close();
+                MessageBox.Show("ATM locations could not be loaded. " + ex.Message,
+                    "Admin - Nearby ATMs", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+        }
+
+        private static DGVPrinter CreatePrinter(string subtitle)
+        {
+            return new DGVPrinter
+            {
+                Title = "\r\n\r\n\r\n ZEMO Bank \r\n\r\n",
+                SubTitle = subtitle + " \r\n\r\n\r\n",
+                SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip,
+                PageNumbers = false,
+                PageNumberInHeader = false,
+                PorportionalColumns = true,
+                HeaderCellAlignment = StringAlignment.Near,
+                Footer = "Admin - ZEMO Bank",
+                FooterSpacing = 15
+            };
         }
     }
 }
