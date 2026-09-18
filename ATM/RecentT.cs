94d4710791de74b9c0247fa9b0664c1a92aa5961
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-18 07:43:44 +0530

    refactor: make account transactions safer

--- ATM/RecentT.cs
+++ ATM/RecentT.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
 namespace ATM
@@ -20,38 +12,26 @@ namespace ATM
 
         private void pbclose_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void btnback_Click(object sender, EventArgs e)
         {
-            this.Close();
-            Dash1 dash = new Dash1();
-            dash.Show();
+            Close();
+            new Dash1().Show();
         }
 
         private void RecentT_Load(object sender, EventArgs e)
         {
             try
             {
-                string connectionString;
-                SqlConnection cnn;
-
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS; Initial Catalog = ZEMO_Bank; User ID = admin; Password = admin";
-
-                cnn = new SqlConnection(connectionString);
-
-                cnn.Open();
-                String sql = "Select*from RecentT where AccNo = '" + Dash1.AccNo + "'";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-
-                SqlDataAdapter ada = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                ada.Fill(dataTable);
-                dataGridView1.DataSource = dataTable;
-                cnn.Close();
+                dataGridView1.DataSource = BankingService.GetRecentTransactions(Dash1.AccNo);
             }
-            catch (Exception ex) { MessageBox.Show("" + ex); }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Transactions could not be loaded. " + ex.Message, "Recent transactions",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
